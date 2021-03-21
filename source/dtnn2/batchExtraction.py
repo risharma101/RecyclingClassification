4c0878617ff428508d98322ebec3fed8a0ef9a71
AndrewJLang <andrewespn@hotmail.com>
2021-03-21 13:30:23 -0700

    changed batch selection

--- source/dtnn2/batchExtraction.py
+++ source/dtnn2/batchExtraction.py
@@ -1,3 +1,24 @@
 import numpy as np
+import os
 
-# We want to handle splitting the training vs testing here
+TRAININGFEATUREPATH = "../featureextractor/featureInfo/training/"
+VALIDATIONFEATUREPATH = "../featureextractor/featureInfo/validation/"
+
+# Get the training batch
+# def getTrainingBatch(batchSize):
+
+
+# Get the validation batch
+def getTestingBatch(batchSize=None):
+    testingPath = os.listdir(VALIDATIONFEATUREPATH)
+    data = []
+    labels = []
+    for featureVector in testingPath:
+        # The first column of all these is the label so we can extract that
+        loadFeatureVector = np.load("{0}{1}".format(VALIDATIONFEATUREPATH, featureVector))
+        featureLabel = [x[0] for x in loadFeatureVector]
+        featureData = loadFeatureVector[:, 1:]
+        data.append(featureData)
+        labels.append(featureLabel)
+    
+    return np.array(data), np.array(labels)
